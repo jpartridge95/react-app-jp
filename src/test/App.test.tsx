b57a46a8dd4276131ce57df7b8f12e4f0bca9251
japar <jordanpartridge@gmail.com>
2022-03-09 02:03:19 +0000

    clean up of unused test file

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+// I had all intentions of writing 'unit' tests but at roughly 2:45 minutes in
+// I have run out of time. I'd have written them first had I not been under
+// quite as much time presure.
